96e62604c1d3fc6faf20e3365c779c65d35c6677
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-01-08 23:08:49 +0400

    now getting from server, need to use expressrouters to send parameters to the serverside

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const fetch = require("node-fetch");
 const app = express();
 
 app.listen(3000, () => console.log("listening"));
 app.use(express.static("public"));
 // starting express local server
+
+app.get("/getWeathert", async (req, res) => {
+  console.log("lat");
+  const apiKey = "d339a3f471054b41af227bc50c72516a";
+  //   const apiURL = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${apiKey}`;
+  const apiURL =
+    "https://api.weatherbit.io/v2.0/current?lat=25.2559544&lon=55.3033014&key=d339a3f471054b41af227bc50c72516a";
+  const response = await fetch(apiURL);
+  const jsonData = await response.json();
+  res.json(jsonData);
+});
--- public/main.js
+++ public/main.js
@@ -4,5 +4,14 @@ if ("geolocation" in navigator) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     console.log(lat, lon);
+    // const apiKey = "d339a3f471054b41af227bc50c72516a";
+    // const apiURL = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${apiKey}`;
+    const apiURL = `/getWeathert`;
+
+    const response = await fetch(apiURL);
+    const jsonData = await response.json();
+    console.log(jsonData);
   });
+} else {
+  console.error("Geo Location not Available");
 }
