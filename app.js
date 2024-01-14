c4698b41425280e58738a44931d9e4f207490958
Hammad Bhatti <hammad.bhatti1996@gmail.com>
2024-01-14 19:41:59 +0400

    Added env file for api keys

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const Datastore = require("nedb");
+require("dotenv").config();
 
 const app = express();
 app.listen(3000, () => console.log("listening"));
@@ -14,13 +15,13 @@ app.get("/getAPIData/:data", async (req, res) => {
   const latLon = req.params.data.split(",");
   const lat = latLon[0];
   const lon = latLon[1];
-  const apiKey = "d339a3f471054b41af227bc50c72516a";
+  const apiKey = process.env.WEATHER_API_KEY;
   const apiURL = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${apiKey}`;
   const response = await fetch(apiURL);
   const tempJson = await response.json();
   // console.log(tempJson.data[0].app_temp);
 
-  const aqApiKey = "c8fc628a5963e966c0674716b3250adb";
+  const aqApiKey = process.env.AQI_API_KEY;
   const aqApiURL = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${aqApiKey}`;
   const aqResponse = await fetch(aqApiURL);
   const aqJsonData = await aqResponse.json();
